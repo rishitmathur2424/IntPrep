fc9d808c63f9c138f34ee906c85be0c2ca00efcd
Rishit Mathur <rishitmathur3@gmail.com>
2026-04-18 09:50:37 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,22 @@
-import os, json
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 firebase_config = os.getenv("FIREBASE_CONFIG")
 
 if not firebase_config:
-    raise ValueError("FIREBASE_CONFIG not set")
+    raise Exception("❌ FIREBASE_CONFIG not found")
 
-cred = credentials.Certificate(json.loads(firebase_config))
+try:
+    cred = credentials.Certificate(json.loads(firebase_config))
+except Exception as e:
+    print("🔥 Firebase JSON ERROR:", e)
+    raise e
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+print("✅ Firestore initialized successfully")
