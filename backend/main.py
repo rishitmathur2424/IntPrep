a01aad42b3b23330fa87c54c548d1d544d9496d4
Rishit Mathur <rishitmathur3@gmail.com>
2026-04-18 12:54:47 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,8 +29,8 @@ _origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=_origins,  
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
